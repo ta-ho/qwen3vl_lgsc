372631277e3de90c335941c33cc151da18b8bf6b
ta-ho <taho43@kaist.ac.kr>
2026-02-24 19:49:36 +0900

    add special tokens

---
+++ qwen-vl-finetune/tools/constants.py
@@ -0,0 +1,12 @@
+ROI_FEAT_TOKEN = "<roi_feat>"
+DEFAULT_TOKENS = {
+    # "image_start"       : "<img>", 
+    # "image_end"         : "</img>", 
+    # "video_start"       : "<vid>",
+    # "video_end"         : "</vid>", 
+    'index_start'       : "<idx>", 
+    'index_end'         : "</idx>", 
+    'expression_start'  : "<exp>", 
+    'expression_end'    : "</exp>", 
+    'grounding'         : "[grounding]"     # for grounded answer
+}
